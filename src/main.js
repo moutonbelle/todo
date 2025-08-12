1fdf1ea9e7d254aedb645197789cd9ef17d6de91
Mouton Belle <219940491+moutonbelle@users.noreply.github.com>
2025-08-12 17:58:51 -0500

    Add fn to move todo from one project to another project

--- src/main.js
+++ src/main.js
@@ -71,6 +71,11 @@ class ToDoSystem {
     updateTodo (projectID, todoID, updates) {
         this.getProjectByID(projectID).getTodoByID(todoID).update(updates);
     }
+
+    moveTodo (oldProjectID, newProjectID, todoID) {
+        this.getProjectByID(newProjectID).addTodo(this.getProjectByID(oldProjectID).getTodoByID(todoID));
+        this.getProjectByID(oldProjectID).removeTodoByID(todoID);
+    }
 }
 
 class ToDoController {
@@ -86,6 +91,8 @@ class ToDoController {
     removeTodo (projectID, todoID) {this.system.removeTodo(projectID, todoID);}
 
     updateTodo (projectID, todoID, updates) {this.system.updateTodo(projectID, todoID, updates);}
+
+    moveTodo (oldProjectID, newProjectID, todoID) {this.system.moveTodo(oldProjectID, newProjectID, todoID)}
 }
 
 class ToDoRenderer {
@@ -106,10 +113,13 @@ let todoController = new ToDoController(todos, renderer);
 
 function testSuite () {
     todoController.addProject("Tiger");
+    todoController.addProject("Dolphin");
     todoController.addTodo(todoController.system.projects[0].id, {title: "Fetch tiger", description: "Go to the jungle, catch the tiger, and return him", dueDate: "05-15-1999", priority: "high"});
     todoController.addTodo(todoController.system.projects[0].id, {title: "Tame parrot", description: "Find a beautiful parrot and slowly charm it with food and pets", dueDate: "12-03-2001", priority: "medium"});
     todoController.renderer.draw(todoController.system);
     todoController.updateTodo(todoController.system.projects[0].id, todoController.system.projects[0].todos[0].id, {description: "Go to the North of India, find a tiger, catch him with bait, and return him, no exceptions", dueDate: "06-16-2023"});
+    todoController.renderer.draw(todoController.system);
+    todoController.moveTodo(todoController.system.projects[0].id, todoController.system.projects[1].id, todoController.system.projects[0].todos[0].id)
     todoController.renderer.draw(todoController.system);
 }
 
